54c18d23bb04f6ef76131f5adc5383c7f6a2673c
seankang <seankang@tencent.com>
2018-05-02 10:24:08 +0800

    compare ip address in two files

---
+++ scripts/compare.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2017/11/7 17:26
+# @Author  : kangxiaoning
+
+import os
+
+
+def compare_ip():
+    """
+    对比两个IP文件差异，将差异打印出来
+    """
+    path_first = r'/home/dbo/workspace/tools/files/src_ip.txt'
+    path_second = r'/home/dbo/workspace/tools/files/dst_ip.txt'
+
+    def get_ip(path):
+        ret = []
+        with open(path) as f:
+            for line in f:
+                ip = line.strip()
+                if ip and not ip.startswith('#'):
+                    ret.append(ip)
+        return ret
+
+    def diff(src_path, dst_path):
+        src = get_ip(src_path)
+        dst = get_ip(dst_path)
+        name_src = os.path.basename(src_path)
+        name_dst = os.path.basename(dst_path)
+        diff_cnt = 0
+        for ip in src:
+            if ip not in set(dst):
+                diff_cnt += 1
+                print(ip)
+        print('\n{0}中有{1}条数据不在{2}\n'.format(name_src, diff_cnt, name_dst))
+
+    diff(path_first, path_second)
+    diff(path_second, path_first)
+
+
+if __name__ == '__main__':
+    compare_ip()
